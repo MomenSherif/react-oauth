c238373ccc34218dab1d9718548a4f19fdec64fa
Momen Sherif <momensherif.2019@gmail.com>
2022-05-06 20:56:51 +0200

    feat: authorization code & implicit flow (custom button)

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as GoogleOAuthProvider } from './GoogleOAuthProvider';
+export { default as GoogleLogin } from './GoogleLogin';
+export { default as googleLogout } from './googleLogout';
+export { default as useGoogleLogin } from './hooks/useGoogleLogin';
+export { default as useGoogleOneTapLogin } from './hooks/useGoogleOneTapLogin';
