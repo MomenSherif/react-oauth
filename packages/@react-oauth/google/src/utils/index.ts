59fd0b3b8f30ba95cc63f58c0dcc5990ff49bec4
Mo'men Sherif <momensherif.2019@gmail.com>
2022-11-01 21:33:40 +0200

    refactor(utils): extract client id extracts only client id from google response

--- packages/@react-oauth/google/src/utils/index.ts
+++ packages/@react-oauth/google/src/utils/index.ts
@@ -3,22 +3,7 @@ import { GoogleCredentialResponse } from '../types';
 export function extractClientId(
   credentialResponse: GoogleCredentialResponse,
 ): string | undefined {
-  try {
-    const clientId =
-      credentialResponse?.clientId ?? credentialResponse?.client_id;
-    if (clientId) {
-      return clientId;
-    }
-
-    if (!credentialResponse?.credential) {
-      return undefined;
-    }
-
-    const payload = JSON.parse(
-      atob(credentialResponse.credential.split('.')[1]),
-    );
-    return payload?.aud;
-  } catch {
-    return undefined;
-  }
+  const clientId =
+    credentialResponse?.clientId ?? credentialResponse?.client_id;
+  return clientId;
 }
