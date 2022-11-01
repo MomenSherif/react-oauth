c9481f90f2ea7a224edc8da25019d15aab026fac
Yuri Burk <yuriburk@gmail.com>
2022-11-01 09:09:24 -0300

    chore: extract clientId from credential

--- packages/@react-oauth/google/src/utils/index.ts
+++ packages/@react-oauth/google/src/utils/index.ts
@@ -1,11 +1,8 @@
 export function extractClientId(credential: string): string | undefined {
   try {
     const payload = JSON.parse(atob(credential.split('.')[1]));
-    if (!payload.aud) {
-      return undefined;
-    }
 
-    return payload.aud;
+    return payload?.aud;
   } catch {
     return undefined;
   }
