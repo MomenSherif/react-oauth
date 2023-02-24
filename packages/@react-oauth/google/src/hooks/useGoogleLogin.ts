582d836cc791ea57052c160fe4d4d0b4ade7fd76
Bruno Finger <bruno.k.finger@gmail.com>
2023-02-24 15:09:09 +0100

    #203: Fix useGoogleLogin to allow credential type of login

--- packages/@react-oauth/google/src/hooks/useGoogleLogin.ts
+++ packages/@react-oauth/google/src/hooks/useGoogleLogin.ts
@@ -1,16 +1,22 @@
-/* eslint-disable import/export */
 import { useCallback, useEffect, useRef } from 'react';
-
 import { useGoogleOAuth } from '../GoogleOAuthProvider';
 import {
-  TokenClientConfig,
-  TokenResponse,
   CodeClientConfig,
   CodeResponse,
-  OverridableTokenClientConfig,
+  CredentialResponse,
+  GoogleCredentialResponse,
+  IdConfiguration,
+  MomentListener,
   NonOAuthError,
+  OverridableTokenClientConfig,
+  TokenClientConfig,
+  TokenResponse,
 } from '../types';
+import { extractClientId } from '../utils';
 
+type ImplicitOnError = (
+  onError: Pick<TokenResponse, 'error' | 'error_description' | 'error_uri'>,
+) => void;
 interface ImplicitFlowOptions
   extends Omit<TokenClientConfig, 'client_id' | 'scope' | 'callback'> {
   onSuccess?: (
@@ -19,17 +25,15 @@ interface ImplicitFlowOptions
       'error' | 'error_description' | 'error_uri'
     >,
   ) => void;
-  onError?: (
-    errorResponse: Pick<
-      TokenResponse,
-      'error' | 'error_description' | 'error_uri'
-    >,
-  ) => void;
+  onError?: ImplicitOnError;
   onNonOAuthError?: (nonOAuthError: NonOAuthError) => void;
   scope?: TokenClientConfig['scope'];
   overrideScope?: boolean;
 }
 
+type AuthCodeOnError = (
+  onError: Pick<TokenResponse, 'error' | 'error_description' | 'error_uri'>,
+) => void;
 interface AuthCodeFlowOptions
   extends Omit<CodeClientConfig, 'client_id' | 'scope' | 'callback'> {
   onSuccess?: (
@@ -38,15 +42,28 @@ interface AuthCodeFlowOptions
       'error' | 'error_description' | 'error_uri'
     >,
   ) => void;
-  onError?: (
-    errorResponse: Pick<
-      CodeResponse,
-      'error' | 'error_description' | 'error_uri'
-    >,
-  ) => void;
+  onError?: AuthCodeOnError;
   onNonOAuthError?: (nonOAuthError: NonOAuthError) => void;
   scope?: CodeResponse['scope'];
   overrideScope?: boolean;
+}
+
+type CredentialOnSuccess = (
+  credentialResponse: Omit<
+    CredentialResponse,
+    'error' | 'error_description' | 'error_uri'
+  >,
+) => void;
+type CredentialOnError = () => void;
+interface CredentialFlowOptions
+  extends Omit<IdConfiguration, 'client_id' | 'scope' | 'callback'> {
+  onSuccess?: CredentialOnSuccess;
+  onError?: CredentialOnError;
+  onNonOAuthError?: (nonOAuthError: NonOAuthError) => void;
+  scope?: CodeResponse['scope'];
+  overrideScope?: boolean;
+  state?: never;
+  promptMomentNotification?: MomentListener;
 }
 
 export type UseGoogleLoginOptionsImplicitFlow = {
@@ -57,15 +74,23 @@ export type UseGoogleLoginOptionsAuthCodeFlow = {
   flow?: 'auth-code';
 } & AuthCodeFlowOptions;
 
+export type UseGoogleLoginOptionsCredentialFlow = {
+  flow?: 'credential';
+} & CredentialFlowOptions;
+
 export type UseGoogleLoginOptions =
   | UseGoogleLoginOptionsImplicitFlow
-  | UseGoogleLoginOptionsAuthCodeFlow;
+  | UseGoogleLoginOptionsAuthCodeFlow
+  | UseGoogleLoginOptionsCredentialFlow;
 
 export default function useGoogleLogin(
   options: UseGoogleLoginOptionsImplicitFlow,
 ): (overrideConfig?: OverridableTokenClientConfig) => void;
 export default function useGoogleLogin(
   options: UseGoogleLoginOptionsAuthCodeFlow,
+): () => void;
+export default function useGoogleLogin(
+  options: UseGoogleLoginOptionsCredentialFlow,
 ): () => void;
 
 export default function useGoogleLogin({
@@ -90,28 +115,39 @@ export default function useGoogleLogin({
   const onNonOAuthErrorRef = useRef(onNonOAuthError);
   onNonOAuthErrorRef.current = onNonOAuthError;
 
+  const promptMomentNotificationRef = useRef(
+    (props as UseGoogleLoginOptionsCredentialFlow).promptMomentNotification,
+  );
+  promptMomentNotificationRef.current = (
+    props as UseGoogleLoginOptionsCredentialFlow
+  ).promptMomentNotification;
+
   useEffect(() => {
     if (!scriptLoadedSuccessfully) return;
 
-    const clientMethod =
-      flow === 'implicit' ? 'initTokenClient' : 'initCodeClient';
+    if (flow !== 'credential') {
+      const clientMethod =
+        flow === 'implicit' ? 'initTokenClient' : 'initCodeClient';
 
-    const client = window?.google?.accounts.oauth2[clientMethod]({
-      client_id: clientId,
-      scope: overrideScope ? scope : `openid profile email ${scope}`,
-      callback: (response: TokenResponse | CodeResponse) => {
-        if (response.error) return onErrorRef.current?.(response);
+      clientRef.current = window?.google?.accounts.oauth2[clientMethod]({
+        client_id: clientId,
+        scope: overrideScope ? scope : `openid profile email ${scope}`,
+        callback: (response: TokenResponse | CodeResponse) => {
+          if (response.error)
+            return (onErrorRef.current as ImplicitOnError | AuthCodeOnError)?.(
+              response,
+            );
 
-        onSuccessRef.current?.(response as any);
-      },
-      error_callback: (nonOAuthError: NonOAuthError) => {
-        onNonOAuthErrorRef.current?.(nonOAuthError);
-      },
-      state,
-      ...props,
-    });
+          onSuccessRef.current?.(response as any);
+        },
+        error_callback: (nonOAuthError: NonOAuthError) => {
+          onNonOAuthErrorRef.current?.(nonOAuthError);
+        },
+        state,
+        ...props,
+      });
+    }
 
-    clientRef.current = client;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [clientId, scriptLoadedSuccessfully, flow, scope, state]);
 
@@ -126,5 +162,40 @@ export default function useGoogleLogin({
     [],
   );
 
-  return flow === 'implicit' ? loginImplicitFlow : loginAuthCodeFlow;
+  const loginCredentialFlow = useCallback(() => {
+    console.log(scriptLoadedSuccessfully, flow);
+
+    if (!scriptLoadedSuccessfully) return;
+    if (flow !== 'credential') return;
+
+    window?.google?.accounts?.id?.initialize({
+      client_id: clientId,
+      callback: (credentialResponse: GoogleCredentialResponse) => {
+        console.log(credentialResponse);
+
+        if (!credentialResponse?.credential) {
+          return (onErrorRef.current as CredentialOnError)?.();
+        }
+
+        const { credential, select_by } = credentialResponse;
+        (onSuccessRef.current as CredentialOnSuccess)?.({
+          credential,
+          clientId: extractClientId(credentialResponse),
+          select_by,
+        });
+      },
+    });
+
+    console.log('will prompt', window?.google?.accounts?.id?.prompt);
+    window?.google?.accounts?.id?.prompt(promptMomentNotificationRef.current);
+  }, [clientId, scriptLoadedSuccessfully, flow]);
+
+  switch (flow) {
+    case 'implicit':
+      return loginImplicitFlow;
+    case 'auth-code':
+      return loginAuthCodeFlow;
+    case 'credential':
+      return loginCredentialFlow;
+  }
 }
