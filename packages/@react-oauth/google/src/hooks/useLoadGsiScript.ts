dfafa3f50911ec39681e225d54a4772001e0e3ca
Daniel Sampaio <dan.faria.sampaio@gmail.com>
2023-03-17 12:02:16 -0300

    fix(useLoadGsiScript): adds nonce to dependency array

--- packages/@react-oauth/google/src/hooks/useLoadGsiScript.ts
+++ packages/@react-oauth/google/src/hooks/useLoadGsiScript.ts
@@ -49,7 +49,7 @@ export default function useLoadGsiScript(
     return () => {
       document.body.removeChild(scriptTag);
     };
-  }, []);
+  }, [nonce]);
 
   return scriptLoadedSuccessfully;
 }
